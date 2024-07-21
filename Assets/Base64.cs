e35eb56de834f1383c87f5ed7b9877880c5ed560
Veeloh <102321623+Veeloh@users.noreply.github.com>
2024-07-21 10:40:37 -0600

    first commit

---
+++ Assets/Base64.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Base64 : MonoBehaviour
+{
+    public string playerInput;
+    public string Output;
+
+    public TMP_InputField input;
+    public TMP_InputField OutputExit;
+
+    public static string Base64Encode(string plainText)
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return System.Convert.ToBase64String(plainTextBytes);
+    }
+
+    public static string Base64Decode(string base64EncodedData)
+    {
+        var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+
+    public void Encode()
+    {
+        Output = Base64Encode(playerInput);
+        Debug.Log(Output);
+    }
+
+    public void Decode()
+    {
+        Output = Base64Decode(playerInput);
+        Debug.Log(Output);
+    }
+
+    public void Update()
+    {
+        playerInput = input.text;
+        OutputExit.text = Output;
+    }
+}
